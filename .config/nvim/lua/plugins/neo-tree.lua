b80ab2fb558f9eb4f487cd2688b1e02dd317afba
Bryan <bryanwyk@gmail.com>
2024-12-28 20:44:10 +1100

    Add git-blame nvim plugin, update ghostty theme to tokyonight

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,16 +1,24 @@
 return {
-    {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+        },
       },
-      config = function() 
-        -- Neo-tree config
-        vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal right<CR>', {})
-      end
-    }
+    },
+    config = function()
+      -- Neo-tree config
+      vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal right<CR>", {})
+    end,
+  },
 }
